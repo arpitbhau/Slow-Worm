3047c8708d40721c034e0a0309295101d44efbc5
arpitbhau <arpitnikhade20@gmail.com>
2025-01-05 23:50:58 +0530

    started work on the project once again. made some main menu stuff too

--- main.py
+++ main.py
@@ -1,1 +1,39 @@
 # Jai Shree Ram
+
+from assets.img.banner import banner
+
+
+def welcome():
+    print(banner)
+    print("""as it says, "slow worm"
+note: make sure you have required permission to run the tool (it's dangerous!)\n\nJai Shree Ram\n""")
+
+def createBlankLine():
+    print("\n")
+
+
+def mainMenu():
+    
+    def printOptList():
+        print("1. Slow Worm")
+        print("2. Web Server Crasher")
+        print("3. Firewall Maker")
+        print("4. CLI")
+        print("5. Exit")
+
+
+
+    createBlankLine()
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        print("Starting...")
+    elif choice == "2":
+        print("Stopping...")
+        exit()
+    else:
+        print("Invalid choice! Try again.\n")
+        mainMenu()
+
+
+welcome()
+mainMenu()
