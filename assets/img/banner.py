d212de231371d105a260695712971e44ede95284
arpitbhau <arpitnikhade20@gmail.com>
2025-01-05 22:26:03 +0530

    upadated readme

--- assets/img/banner.py
+++ assets/img/banner.py
@@ -1,6 +1,15 @@
+# Jai Shree Ram
 
-JAI SHREE RAM                                             
-                                                     
+banner = """                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
                  ██████  ██▓     ▒█████   █     █░                  
                ▒██    ▒ ▓██▒    ▒██▒  ██▒▓█░ █ ░█░                  
                ░ ▓██▄   ▒██░    ▒██░  ██▒▒█░ █ ░█                   
@@ -21,11 +30,13 @@ JAI SHREE RAM
                                 ░   ░  ░ ░ ░ ▒    ░░   ░ ░      ░   
                                   ░        ░ ░     ░            ░   
                                                                     
-Slow Worm is a Computer Worm which Multiplies itself and also Destroy the System at the Same time at the Speed which You Can't Imagine.
-website.
-
-There are many Modules of Slow Worm which are being made continuously So Stay tunned!
-
-Disclaimer - This Tool is Very Dangerous So Do it on your Risk.
-
-This Malware is made for good purposes not bad so if the malware is used for unethical activities I will Not be Responsible.
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    
+                                                                    """
